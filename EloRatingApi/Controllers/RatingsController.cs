86176943c9b31d88d7124afba9b287cf6f9852c6
Mattias Boustedt <mattias.boustedt@key-component.com>
2017-07-30 23:27:33 +0200

    Initial commit. Making an api for Elo Rating for Azure

---
+++ EloRatingApi/Controllers/RatingsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+using EloRatingApi.Models;
+using Swashbuckle.Swagger.Annotations;
+using Newtonsoft.Json;
+
+namespace EloRatingApi.Controllers
+{
+    public class RatingsController : ApiController
+    {
+        [SwaggerOperation("CalculateNewRating")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        public string Get(double ratingA, double ratingB, double scoreA, double scoreB)
+        {
+            Rating rating = new Rating(ratingA, ratingB, scoreA, scoreB);
+            List<double> ratingsList = rating.GetNewRatings();
+
+            Result result = new Result()
+            {
+                RatingA = ratingsList[0],
+                RatingB = ratingsList[1]
+            };
+
+            List<double> ratingChanges = result.CalculateRatingChanges(ratingA, ratingB, result.RatingA, result.RatingB);
+
+            result.RatingChangeA = ratingChanges[0];
+            result.RatingChangeB = ratingChanges[1];
+
+            string finalResult = JsonConvert.SerializeObject(result);
+
+            return finalResult;
+        }
+    }
+}
---
+++ EloRatingApi/Models/Result.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace EloRatingApi.Models
+{
+    public class Result
+    {
+        private double ratingA;
+        private double ratingB;
+        private double ratingChangeA;
+        private double ratingChangeB;
+
+        public double RatingA { get { return ratingA; } set { ratingA = value; } }
+        public double RatingB { get { return ratingB; } set { ratingB = value; } }
+        public double RatingChangeA { get { return ratingChangeA; } set { ratingChangeA = value; } }
+        public double RatingChangeB { get { return ratingChangeB; } set { ratingChangeB = value; } }
+
+        public List<double> CalculateRatingChanges(double oldRatingA, double oldRatingB, double newRatingA, double newRatingB)
+        {
+            if (oldRatingA > newRatingA)
+                ratingChangeA = newRatingA - oldRatingA;
+
+            if (newRatingA > oldRatingA)
+                ratingChangeA = newRatingA - oldRatingA;
+
+            if (newRatingA == oldRatingA)
+                ratingChangeA = 0;
+
+
+            if (oldRatingB > newRatingB)
+                ratingChangeB = newRatingB - oldRatingB;
+
+            if (newRatingB > oldRatingB)
+                ratingChangeB = newRatingB - oldRatingB;
+
+            if (newRatingB == oldRatingB)
+                ratingChangeA = 0;
+
+            return new List<double>
+            {
+                ratingChangeA,
+                ratingChangeB
+            };
+
+        }
+    }
+}
